c3641a42756a5321b3a5f6be349a0da98575f029
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-10-12 16:41:43 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
+input.onPinPressed(TouchPin.P1, function () {
+    if (show) {
+        show = 0
+        basic.showIcon(IconNames.Yes)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else {
+        show = 1
+        basic.showIcon(IconNames.No)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
 })
+let show = 0
+basic.showIcon(IconNames.Happy)
+show = 0
+pins.digitalWritePin(DigitalPin.P0, 0)
